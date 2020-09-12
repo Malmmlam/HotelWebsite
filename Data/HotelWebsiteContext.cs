a7ffab688f30f455e4c9eebf09b3d8a212b508ef
Tayyebe-h <t.hoseini1796@gmail.com>
2020-09-12 17:36:39 +0200

    Adding Room model class and scaffold it + Initial migration

---
+++ Data/HotelWebsiteContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HotelWebsite.Models;
+
+namespace HotelWebsite.Data
+{
+    public class HotelWebsiteContext : DbContext
+    {
+        public HotelWebsiteContext (DbContextOptions<HotelWebsiteContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HotelWebsite.Models.Room> Room { get; set; }
+    }
+}
---
+++ Migrations/20200912151412_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HotelWebsite.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Room",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Code = table.Column<string>(nullable: true),
+                    Type = table.Column<string>(nullable: true),
+                    Vacant = table.Column<bool>(nullable: false),
+                    Price = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Room", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Room");
+        }
+    }
+}
